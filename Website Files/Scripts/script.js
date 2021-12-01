cc854e76f0674a1a95ad8c63308185eee65153ef
khalil alsulaimani <alsulaimani.2000@outlook.com>
2021-12-01 15:24:27 +0300

    added the empty , greater then 6 and checking for numbers to the full name validation and added the needed error message for when user types a number

--- Website Files/Scripts/script.js
+++ Website Files/Scripts/script.js
@@ -3,29 +3,65 @@ const password = document.getElementById("loginPassword");
 const loginMessage = document.getElementById("loginMessage");
 const IndexForm = document.getElementById("indexForm");
 
-
 // validation for the index form 
-IndexForm.addEventListener("submit", (e) => {
+function signIn() {
+    loginMessage.style.color = "red";
+    loginMessage.style.fontSize = "20px";
     if (username.value == "" || username.value == null) {
-        loginMessage.style.color = "red";
+
         loginMessage.innerText = "Empty Username Detected";
-        e.preventDefault();
+        return false;
     } else if (password.value === "" || password.value == null) {
         loginMessage.innerText = "Empty Password Detected";
-        e.preventDefault();
+        return false;
     } else if (username.value.length < 4) {
-        loginMessage.innerText = "Username Must Be At Least 4 characters Given Was " + username.value.length;
-        e.preventDefault();
+        loginMessage.innerText = "Username Must Be At Least 4 Characters Given Was " + username.value.length;
+        return false;
     } else if (password.value.length < 6) {
         loginMessage.innerText =
             "Password Must Be At Least 6 characters Given Was " +
             password.value.length;
-        e.preventDefault();
+        return false;
     }
-});
+    return true;
+}
 
 
+// validating feedback page
 
+const fullName = document.getElementById("names");
+const feedbackMessage = document.getElementById("feedbackMessage");
+const keyUpMessage = document.getElementById("keyUpMessage");
+
+function FeedbackForm() {
+    feedbackMessage.style.color = "red";
+    feedbackMessage.style.fontSize = "20px";
+    if (fullName.value == "" || fullName.value == null) {
+        feedbackMessage.innerText = "Empty Full Name";
+        return false;
+    } else if (fullName.value.length < 6) {
+        feedbackMessage.innerText = "Full Name Must Be At Least 6 Characters Given Was " + fullName.value.length;
+        return false;
+    }else if(checkName){
+        return false;
+    }
+    return true;
+}
+
+function checkName() {
+    keyUpMessage.innerText="";
+    keyUpMessage.style.color = "red";
+    keyUpMessage.style.fontSize = "15";
+    var regEx = new RegExp("\\d+");
+    if (regEx.test(fullName.value)){
+        keyUpMessage.innerText = "Numbers Cannot Be In Name";
+        return false;
+    }
+
+}
+
+
+// changing textBox/fields to yellow and white 
 function focused(id) {
     id.style.backgroundColor = "yellow";
 }
